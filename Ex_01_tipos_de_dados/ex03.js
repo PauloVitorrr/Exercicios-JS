cd0c8d0f2e8da7784bd8447c0ac5c6d6ea1835bb
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-18 19:21:49 -0300

    exercicio boolean

---
+++ Ex_01_tipos_de_dados/ex03.js
@@ -0,0 +1,11 @@
+//Escreva tres comparações com boolean. Uma com maior, menor ou igual e diferente;
+
+console.log(5 > 3)
+console.log(10 < 5)
+console.log("798" != 798)
+
+/*
+true
+false
+false
+*/
