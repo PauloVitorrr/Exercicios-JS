53a094b75864d1102ff3aa5d3390dfe03d45926c
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-18 18:47:28 -0300

    Exercicio sobre strings

---
+++ Ex_01_tipos_de_dados/ex01.js
@@ -0,0 +1,12 @@
+// Escreva tres valores em string, com aspas duplas, simples e literels.
+
+console.log("Olá mundo")
+console.log('Curso de javascript!!!')
+console.log(`Esse é o tipo literals`)
+
+
+/*
+Olá mundo
+Curso de javascript!!!
+Esse é o tipo literals
+*/
