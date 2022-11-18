b6a4b4fe073991a8f48725a799763eed98df4a75
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-18 19:22:10 -0300

    exercicio operadores logicos

---
+++ Ex_01_tipos_de_dados/ex04.js
@@ -0,0 +1,11 @@
+// Escreva tres comparações com operadores lógicos com and, or e not;
+
+console.log(3 == 3 && 4 > 1)
+console.log(4 <= 2 || "Matheus" == "Pedro")
+console.log(!(2 == 2))
+
+/*
+true
+false
+false
+*/
