333fb78e844b357999dc9495bcbb65060f939180
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-18 18:52:28 -0300

    exercicios sobre numbers

---
+++ ex02.js
@@ -0,0 +1,12 @@
+// Escreva tres valores em number em um arquivo e exiba no navegador com números inteiros, números com casa decimal e por aritmética;
+
+console.log(7)
+console.log(10.1)
+console.log((3 * (12 +4))/ 8)
+
+
+/*
+7
+10.1
+6
+*/
