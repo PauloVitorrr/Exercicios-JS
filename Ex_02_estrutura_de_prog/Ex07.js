c50c790503568feec5d062be802ee189cb77e671
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-20 15:27:55 -0300

    estrutura if

---
+++ Ex_02_estrutura_de_prog/Ex07.js
@@ -0,0 +1,11 @@
+//Cria uma estrutura if que verifica a entrada na balada, se tiver mais de 18 anos pode entrar
+
+const Pedro = 18
+
+if(Pedro >= 18){
+    console.log("Pode entrar")
+} else {
+    console.log("Não pode entrar")
+}
+
+// Pode entrar
