4d75ca65a220b1b8bc1c49f947c764aedf43cb13
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-20 15:59:01 -0300

    condição if e else

---
+++ Ex_02_estrutura_de_prog/Ex10.js
@@ -0,0 +1,13 @@
+// verifique se o carro está na velocidade permitida, velocidade max = 80Km
+
+const Km = 80
+
+if(Km > 80){
+    console.log("Dirija com atenção, voce levou uma multa!")
+} else {
+    console.log("Voce está na velocidade permitida!!!!")
+}
+
+/**
+ Voce está na velocidade permitida!!!!
+ */
