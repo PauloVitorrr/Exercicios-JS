88b6465367baa6a89f422ffb86bc452bd553a469
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-20 15:27:30 -0300

    armazenando em variaveis

---
+++ Ex_02_estrutura_de_prog/Ex06.js
@@ -0,0 +1,16 @@
+// Armazene em valores em váriaveis com cada um dos tipos de dados vistos
+
+const Number = 70
+console.log(Number)
+
+const Name = "Paulo"
+console.log(Name)
+
+const Boolean = true
+console.log(Boolean)
+
+/*
+70
+Paulo
+true
+*/
