d38516ba244dc5ce2023719aa2165e4efce75845
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-20 15:28:17 -0300

    estruta if com variavel string

---
+++ Ex_02_estrutura_de_prog/Ex08.js
@@ -0,0 +1,9 @@
+//Crie uma variavel com seu nome como valor, depois verifique se o nome é compativel
+
+const name = "Paulo"
+
+if(name === "Paulo"){
+    console.log(`Olá ${name}`)
+} else {
+    console.log("Usuario não identificado!!")
+}
