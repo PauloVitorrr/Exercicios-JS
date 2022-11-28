5513c577adc3f471d94592de7fdb0f25365d2ac7
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-11-28 19:27:17 -0300

    ex de numeros impares/pares com for

---
+++ Ex_02_estrutura_de_prog/Ex14.js
@@ -0,0 +1,10 @@
+//escreva um looop for ou while que exiba qual número é par e qual número é impar
+
+for(var i= 0; i <= 50; i++){
+
+    if(i % 2 == 0){
+        console.log(`o número ${i} é par`)
+    }else{
+        console.log(`o número ${i} é impar`)
+    }
+}
